50af675203135662eb56f9a1a7adf53b4c53cc27
hosseinrouhi79 <hosseinrouhi79@gmail.com>
2023-01-21 17:55:10 +0330

    login user with sanctum

--- app/Http/Controllers/customer/AuthController.php
+++ app/Http/Controllers/customer/AuthController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\customer;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CustomerloginRequest;
 use App\Http\Requests\CustomerRequest;
 use App\Models\User;
 use App\Trait;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
@@ -26,6 +28,22 @@ class AuthController extends Controller
         ],'Registeration is successfull');
 
     }
+
+    public function login(CustomerLoginRequest $request)
+    {
+        $request->validated($request->all());
+        if (!Auth::attempt($request->only(['email','password']))){
+            return $this->error('','Credential is not valid',401);
+        }
+        else {
+            $user = User::where('email',$request->email)->first();
+            return $this->success(['user'=>$user,
+                'token'=>$user->createToken('Api token of '.$user->name)->plainTextToken],
+            'You are logged in');
+        }
+
+    }
+
 
 }
 //sudo /opt/lampp/lampp start
