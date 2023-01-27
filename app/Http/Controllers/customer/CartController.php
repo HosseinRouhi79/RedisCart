113422ab5d982c451b85a04e30eb5bf18a753ab7
Hossein <hosseinrouhi79@gmail.com>
2023-01-27 14:11:38 +0330

    add phone to cart Redis

--- app/Http/Controllers/customer/CartController.php
+++ app/Http/Controllers/customer/CartController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\customer;
 use App\Http\Controllers\Controller;
 use App\Models\Phones;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class CartController extends Controller
 {
@@ -14,5 +15,13 @@ class CartController extends Controller
         return response()->json([
            'phones'=>$phones
         ]);
+    }
+
+    public function addToCart(Phones $phone)
+    {
+        Redis::lpush('cart',$phone->name);
+
+        $values = Redis::lrange('cart',0,-1);
+        return $values;
     }
 }
